4e782c0b69b633dbcb5ad49b8eddfdb6e2ff94e4
Wen-Hong Huang <wayne123tw@gmail.com>
2015-12-18 17:40:33 +0800

    add js-route and pwd

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/login'  => 'pages#login',   :as => 'login'
   get '/main'   => 'pages#main',    :as => 'main'
 
+  resources :user, path: '/login/user'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
