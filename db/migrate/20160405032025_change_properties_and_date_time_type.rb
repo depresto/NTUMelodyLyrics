37c442ba222ab000875b02d2f2a2a06eef7a7d5f
Wen-Hong Huang <wayne123tw@gmail.com>
2016-04-05 22:25:28 +0800

    debugs migration 2

--- db/migrate/20160405032025_change_properties_and_date_time_type.rb
+++ db/migrate/20160405032025_change_properties_and_date_time_type.rb
@@ -7,9 +7,9 @@ class ChangePropertiesAndDateTimeType < ActiveRecord::Migration
   	add_column :club_properties, :deadline, :date
   	remove_column :club_properties, :p_id
 
-  	change_column :calendars, :starttime, :time
-  	change_column :calendars, :endtime, :time
-  	change_column :calendars, :startdate, :date
-  	change_column :calendars, :enddate, :date
+  	#change_column :calendars, :starttime, :time
+  	#change_column :calendars, :endtime, :time
+  	#change_column :calendars, :startdate, :date
+  	#change_column :calendars, :enddate, :date
   end
 end
