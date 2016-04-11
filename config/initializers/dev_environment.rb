2f79a4fb944fec8c9eeebdc42c8f00a360f2f700
Wen-Hong Huang <wayne123tw@gmail.com>
2016-04-12 00:54:03 +0800

    borrow item theme

---
+++ config/initializers/dev_environment.rb
@@ -0,0 +1,6 @@
+unless Rails.env.production?
+	ENV['CLIENT_ID'] = 'b024039e0ad912a'
+	ENV['CLIENT_SECRET'] = '823e5b5fb6bfcb44c10efae8ac2e1ab4ffbeaf05'
+	ENV['ACCESS_TOKEN'] = '866b5d072154209040c54b2da6658aa54d5f2c83'
+	ENV['REFRESH_TOKEN'] = '22dfc4f33e835cb744e37be8f2852f84da6d23e2'
+end
